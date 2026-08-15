14371d0ab92a04e3787992f5497d1a3788fb97eb
Zachary Richardson <richardson.zach.076@gmail.com>
2026-08-14 19:04:58 -0700

    renaming input to direction

--- character.gd
+++ character.gd
@@ -4,8 +4,8 @@ extends CharacterBody2D
 @export var health: int = 100
 
 func _physics_process(_delta: float) -> void:
-	var input := Vector2.ZERO
-	input.x = Input.get_axis("move_left", "move_right")
-	input.y = Input.get_axis("move_up", "move_down")
-	velocity = input.normalized() * speed
+	var direction := Vector2.ZERO
+	direction.x = Input.get_axis("move_left", "move_right")
+	direction.y = Input.get_axis("move_up", "move_down")
+	velocity = direction.normalized() * speed
 	move_and_slide()
